e99c5ecdfb59bcbf4b34b9fbdbf172067ba45788
Scott Sickles <scottsickles@gmail.com>
2024-03-20 20:40:18 -0400

    reset to beginning

--- README.md
+++
@@ -1,4 +0,0 @@
-Produces an AWS Batch Compute Environment.
-
-## Usage
-TODO: Add usage instructions here
--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "compute_environment_arn" {
-  value       = aws_batch_compute_environment.this.arn
-  description = "string ||| AWS Arn for the compute environment."
-}
-
-output "compute_environment_name" {
-  value       = aws_batch_compute_environment.this.compute_environment_name
-  description = "string ||| Name of the compute environment."
-}

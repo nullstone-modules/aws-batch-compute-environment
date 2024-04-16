0ed8f1cc524bafd43547722405c8c85a0e478c8c
Scott Sickles <scottsickles@gmail.com>
2024-04-16 16:22:36 -0400

    output the security group id

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "compute_environment_name" {
   value       = aws_batch_compute_environment.this.compute_environment_name
   description = "string ||| Name of the compute environment."
 }
+
+output "security_group_id" {
+  value       = aws_security_group.this.id
+  description = "string ||| The ID of the security group for controlling access privileges."
+}
